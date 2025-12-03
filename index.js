4759a10a57b318e7501ea4d682a2c153b26d3940
Karthik Nair <karthiknair@ufl.edu>
2025-12-03 17:59:39 -0500

    Replace index.js and tracing.js with exact assignment code

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
-const express = require('express');
-const { MongoClient } = require('mongodb');
-const app = express();
-const port = 3000;
-
-const url = "mongodb://localhost:27017/";
-const dbName = "todo";
-
-MongoClient.connect(url, (err, client) => {
-  if (err) throw err;
-
-  const db = client.db(dbName);
-  const todoCollection = db.collection("todo");
-
-  // Seed initial data
-  todoCollection.insertMany([
-    { id: 1, title: "Task 1", description: "Description 1" },
-    { id: 2, title: "Task 2", description: "Description 2" },
-    { id: 3, title: "Task 3", description: "Description 3" }
-  ], (err) => {
-    if (err) throw err;
-    console.log("Sample data inserted");
-  });
-
-  //
+const express = require("express"); 
+const { MongoClient } = require("mongodb"); 
+const app = express(); 
+app.use(express.json()); 
+const port = 3000; 
+let db; 
+ 
+const startServer = async () => { 
+   const client = await MongoClient.connect("mongodb://localhost:27017/"); 
+   db = client.db("todo"); 
+   await db.collection("todos").insertMany([ 
+       { 
+           id: "1" 
+           , title: "Buy groceries" 
+       }, 
+       { 
+           id: "2" 
+           , title: "Install Aspecto" 
+       }, 
+       { 
+           id: "3" 
+           , title: "buy my own name domain" 
+       }, 
+   ]); 
+   app.listen(port, () => { 
+       console.log(`Example app listening on port ${port}`); 
+   }); 
+}; 
+startServer(); 
+ 
+app.get("/todo", async (req, res) => { 
+   const todos = await db.collection("todos").find({}).toArray() 
+   res.send(todos) 
+}); 
+ 
+ 
+app.get("/todo/:id" 
+, async (req, res) => { 
+const todo = await db.collection("todos").findOne({ id: req.params.id }); 
+res.send(todo); 
+});
